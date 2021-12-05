a1249b52f5009ada0502dd693d95decfbc70fa99
Nick Freear <nfreear@yahoo.co.uk>
2021-12-05 18:14:55 +0000

    Adding an `index.js` Javascript - used in demos + `main` in package JSON

---
+++ index.js
@@ -0,0 +1,11 @@
+/*!
+
+  NDF, 04-Dec-2021.
+*/
+
+export { MyMap } from './src/components/my-map.js';
+export { MyStarRatingElement } from './src/components/MyStarRatingElement.js';
+
+export { MyElement } from './src/MyElement.js';
+
+export { translate } from './src/translate.js';
