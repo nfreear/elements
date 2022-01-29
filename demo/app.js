1b86cd4b7f965d3d32f71a68b1b9b2e817da6860
Nick Freear <nfreear@yahoo.co.uk>
2022-01-29 09:23:28 +0000

    feat: Add `demo/app` Javascript - demo of `<my-text-to-speech>` (#10)

---
+++ demo/app.js
@@ -0,0 +1,16 @@
+/**
+ * Javascript specific to the Demo.
+ *
+ * @copyright © Nick Freear, 27-Jan-2022.
+ */
+
+import '../index.js';
+
+// For "my-speech.html" ~ <my-text-to-speech>
+const queryString = window.location.href;
+
+const mtLang = queryString.match(/lang=(en|es|fr|zh)(\-[\w]+)?/);
+// const mtVox = queryString.match(/vox=([\w]+)/)
+const LANG = mtLang ? mtLang[1] + (mtLang[2] || '') : 'en';
+
+document.body.setAttribute('data-tts-lang', LANG);
