f2e62f7fde9ef7c54d6c0065e05c6f6fe22be493
Nick Freear <nfreear@yahoo.co.uk>
2022-01-29 09:43:27 +0000

    fix: Add CSS styles for <details> & <summary> (#10)
    * Plus lint fix for `demo/app` Javascript

--- demo/app.js
+++ demo/app.js
@@ -9,7 +9,7 @@ import '../index.js';
 // For "my-speech.html" ~ <my-text-to-speech>
 const queryString = window.location.href;
 
-const mtLang = queryString.match(/lang=(en|es|fr|zh)(\-[\w]+)?/);
+const mtLang = queryString.match(/lang=(en|es|fr|zh)(-[\w]+)?/);
 // const mtVox = queryString.match(/vox=([\w]+)/)
 const LANG = mtLang ? mtLang[1] + (mtLang[2] || '') : 'en';
 
