669237e5b411943cce61486bbec736f308e44233
Nick Freear <nfreear@yahoo.co.uk>
2022-07-31 09:17:14 +0100

    fix: Update to <my-busy-spinner> element

--- src/components/MyBusySpinnerElement.js
+++ src/components/MyBusySpinnerElement.js
@@ -4,6 +4,7 @@
  * @copyright © Nick Freear, 2021-10-26.
  *
  * @see https://gist.github.com/nfreear/ae83b11a7572cd3122a181bb6ebb7fff,
+ * @see https://gist.github.com/nfreear/a71bfce05ef9bae03011ea33762c9d69,
  * @see https://sap.github.io/ui5-webcomponents/playground/components/BusyIndicator/,
  * @see https://github.com/SAP/ui5-webcomponents/blob/master/packages/main/src/BusyIndicator.hbs,
 */
@@ -15,10 +16,20 @@ export class MyBusySpinnerElement extends MyElement {
     return 'my-busy-spinner';
   }
 
-  async connectedCallback () {
+  static get observedAttributes () {
+    return ['hidden'];
+  }
+
+  attributeChangedCallback (name, oldValue, newValue) {
+    const VALUE = newValue !== null;
+
+    console.debug(`my-busy-spinner ~ ${name}: ${VALUE} ('${oldValue}', '${newValue}')`);
+  }
+
+  async __connectedCallback () {
     // const name = this.getAttribute('name') || 'A name attribute';
 
-    await this.getTemplate('my-busy-spinner');
+    /** @WAS await this.getTemplate('my-busy-spinner'); */
 
     console.debug('my-busy-spinner:', this);
   }
