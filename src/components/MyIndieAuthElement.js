705919d8b6a9672ff777a67aeac10630345cc907
Nick Freear <nfreear@yahoo.co.uk>
2022-10-14 19:44:26 +0100

    feat: Complete initial version of <my-indie-auth> (#10)

--- src/components/MyIndieAuthElement.js
+++ src/components/MyIndieAuthElement.js
@@ -8,7 +8,7 @@
 
 import { MyElement } from '../MyElement.js';
 
-const { fetch, location, sessionStorage, URLSearchParams } = window;
+const { crypto, fetch, location, postMessage, sessionStorage, URLSearchParams } = window;
 
 /* const KEY_STATE = 'my-indie-auth.state';
 const KEY_SUBMIT = 'my-indie-auth.submit';
@@ -17,14 +17,28 @@ const KEY_LOGOUT = 'my-indie-auth.logout';
 */
 
 const TEMPLATE = `
+<style>
+  form p > * { line-height: 1.5; margin: .4rem 0; }
+  button,input { font: inherit; }
+  button { padding: .3rem 1.7rem; }
+  input { padding: .3rem; width: 13rem; }
+  a[ href *= indieauth ] { float: right; margin: .5rem; text-decoration: none; }
+  a[ href *= indieauth ] > * { text-decoration: underline; }
+  a[ href *= indieauth ]::after { content: '🔒'; margin-left: .2rem; }
+</style>
+<p role="alert"></p>
 <form action="https://indielogin.com/auth" method="get">
-  <label for="url">Web Address:</label>
-  <input id="url" type="url" name="me" placeholder="yourdomain.com" required />
-  <p><button type="submit">Sign In</button></p>
+  <p>
+    <label for="url">Web Address</label>
+    <input id="url" type="url" name="me" placeholder="yourdomain.com" required />
+    <button type="submit">Sign In</button>
+    <a href="https://indieauth.net/" title="OAuth for the open web"><small>IndieAuth</small></a>
+  </p>
   <input type="hidden" name="client_id" value="https://example.com/" />
   <input type="hidden" name="redirect_uri" value="https://example.com/redirect" />
   <input type="hidden" name="state" value="[todo]" />
 </form>
+<div id="wrap" hidden><slot><!-- Logged in content. --></slot></div>
 `;
 
 export class MyIndieAuthElement extends MyElement {
@@ -33,6 +47,7 @@ export class MyIndieAuthElement extends MyElement {
   }
 
   async connectedCallback () {
+    this.$$ = {};
     // const name = this.getAttribute('name') || 'A name attribute';
 
     const ROOT = document.createElement('div');
@@ -41,11 +56,21 @@ export class MyIndieAuthElement extends MyElement {
     this.attachShadow({ mode: 'open' }).appendChild(ROOT);
 
     this._initializeLoginForm();
+
+    if (this._authenticated) {
+      this._success();
+    } else {
+      this._processAuthRedirect();
+    }
   }
 
   _initializeLoginForm () {
     // const MY_LOGIN = document.querySelector('my-login');
-    const FORM = this.shadowRoot.querySelector('form');
+    this.$$.rootElem = document.documentElement;
+    this.$$.statusElem = this.shadowRoot.querySelector('[ role = alert ]');
+    this.$$.wrapElem = this.shadowRoot.querySelector('#wrap');
+    this.$$.innerElem = this.children[0]; // this.parentElement.querySelector('my-indie-auth > *');
+    const FORM = this.$$.formElem = this.shadowRoot.querySelector('form');
     const ELEMS = FORM.elements;
 
     const client_id = location.origin + '/';
@@ -56,8 +81,8 @@ export class MyIndieAuthElement extends MyElement {
       client_id,
       redirect_uri,
       action: FORM.action,
-      state: crypto.randomUUID(), // += parseInt(Math.random() * 1000000);
-    }
+      state: crypto.randomUUID() // += parseInt(Math.random() * 1000000);
+    };
 
     ELEMS.client_id.value = DATA.client_id;
     ELEMS.redirect_uri.value = DATA.redirect_uri;
@@ -72,7 +97,7 @@ export class MyIndieAuthElement extends MyElement {
       console.debug('my-indie-auth - Submit:', DATA, ev, ev.target, ELEMS);
     });
 
-    console.debug('my-indie-auth:', DATA, this);
+    console.debug('my-indie-auth - Form:', DATA, this.$$, this);
   }
 
   _blurHandler (ev) {
@@ -85,7 +110,7 @@ export class MyIndieAuthElement extends MyElement {
     console.debug('my-indie-auth - Blur:', ELEM.value, ev);
   }
 
-  _processAuthRedirect () {
+  async _processAuthRedirect () {
     const STATE = this._getItem('state');
     const QRY = new URLSearchParams(location.search);
 
@@ -99,9 +124,107 @@ export class MyIndieAuthElement extends MyElement {
         client_id: DATA.client_id
       });
 
-      console.debug('MATCH! ::', DATA, BODY.toString(), QRY);
-      return;
+      console.debug('my-indie-auth - Match:', DATA, BODY.toString(), QRY);
+
+      try {
+        const RESP = await fetch(DATA.action, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            Accept: 'application/json'
+          },
+          body: BODY.toString()
+        });
+        const { status, ok } = RESP;
+        const { error, error_description, me } = await RESP.json();
+
+        console.debug('Auth resp:', ok, status, RESP);
+
+        if (!ok) {
+          /* error : "invalid_request"
+          message : "The authorization code expired"
+          status : 400 */
+          return this._error(status, new Error(error_description));
+        }
+
+        if (ok) {
+          return this._success(status, me);
+        }
+      } catch (ex) {
+        return this._error(999, ex);
+      }
+      // this._error(500, { message: 'Unknown' });
+    } else {
+      console.debug('my-indie-auth - No auth redirect.');
     }
+  }
+
+  // const rootElem = document.documentElement;
+  _success (httpStatus, me, already = false) {
+    const message = `${this._authenticated ? 'Already a' : 'A'}uthenticated`;
+    const AUTH = this._authenticated
+      ? this._getAuth()
+      : {
+          httpStatus,
+          me,
+          message,
+          time: new Date().toISOString()
+        };
+
+    if (!this._authenticated) {
+      this._setItem('auth', JSON.stringify(AUTH));
+    }
+
+    // this.dataset = AUTH;
+    this.dataset.me = AUTH.me;
+
+    this.$$.rootElem.dataset.myIndieAuthMe = AUTH.me;
+    this.$$.rootElem.dataset.myIndieAuthenticated = AUTH.time;
+    this.$$.formElem.hidden = true;
+    this.$$.wrapElem.hidden = false;
+    this.$$.statusElem.textContent = `✔️ ${message}.`;
+
+    if (this.$$.innerElem) {
+      this.$$.innerElem.hidden = false;
+    }
+
+    this._postMessage('success', this._getAuth());
+
+    console.debug(`✔️ my-indie-auth - ${message}.`, me);
+  }
+
+  _error (httpStatus, error) {
+    this.$$.rootElem.dataset.myIndieAuthHttpStatus = httpStatus;
+    this.$$.rootElem.dataset.myIndieAuthError = true;
+    this.$$.rootElem.dataset.myIndieAuthenticated = false;
+    this.$$.statusElem.textContent = '❌ Authentication Error.';
+
+    this._postMessage('error', error);
+
+    console.error('❌ my-indie-auth - Error:', { error });
+  }
+
+  _postMessage (status, data) {
+    const { origin } = location;
+    return postMessage({ tag: MyIndieAuthElement.getTag(), status, data }, origin);
+  }
+
+  _storeAuth (httpStatus, me) {
+    this._setItem('auth', JSON.stringify({
+      httpStatus,
+      me,
+      message: 'Authenticated',
+      time: new Date().toISOString()
+    }));
+  }
+
+  get _authenticated () {
+    return !!this._getItem('auth');
+  }
+
+  _getAuth () {
+    const AUTH = this._getItem('auth');
+    return AUTH ? JSON.parse(AUTH) : null;
   }
 
   _removeItems () {
