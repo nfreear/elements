0b461d6998e702a9a861222463f1c2684c15a1af
Nick Freear <nfreear@yahoo.co.uk>
2022-11-23 20:27:28 +0000

    feat: Enhancements to `<my-test>` - add JSON output (#34)

--- src/components/MyTestElement.js
+++ src/components/MyTestElement.js
@@ -8,7 +8,9 @@
 
 import { MyElement } from '../MyElement.js';
 
-const { Node } = window;
+const { fetch, FormData, Node, Request } = window;
+
+const filterStrings = ['tag seen', 'Stray end tag', 'Bad start tag', 'violates nesting rules', 'Duplicate ID', 'first occurrence of ID', 'Unclosed element', 'not allowed as child of element', 'unclosed elements', 'not allowed on element', 'unquoted attribute value', 'Duplicate attribute'];
 
 const TEMPLATE = `
 <template>
@@ -16,14 +18,17 @@ const TEMPLATE = `
   <style>
     form { margin: 1rem 0; }
     button { font: inherit; padding: .4rem .75rem; }
+    label { margin-right: 1rem; }
   </style>
   <form method="POST" action="https://validator.w3.org/nu/"
     target="_blank" acceptCharset="utf-8" enctype="multipart/form-data">
     <button type="submit">Check me! 💙</button>
     <details>
       <summary>(data)</summary>
-      <input name="showsource" value="yes"/>
-      <textarea name="content"></textarea>
+      <label>Source? <input name="showsource" value="yes" checked type="checkbox"></label>
+      <label>Outline? <input name="showoutline" value="yes" checked type="checkbox"></label>
+      <label>JSON? <input name="out" value="json" checked type="checkbox"></label>
+      <label>Content <textarea name="content"></textarea></label>
     </details>
   </form>
 </template>
@@ -35,18 +40,64 @@ export class MyTestElement extends MyElement {
   }
 
   connectedCallback () {
-    // .
+    this.$$ = {};
     this._attachLocalTemplate(TEMPLATE);
 
-    const FORM = this.shadowRoot.querySelector('form');
+    this.$$.form = this.shadowRoot.querySelector('form');
 
-    FORM.elements.content.value = this._getDocument(document);
+    // FORM.elements.content.value = this._getDocument(document);
 
     // Facilitate Bootstrap ;).
     this.style.display = 'block';
     this.classList.add('container');
 
+    this.$$.form.addEventListener('submit', ev => this._handleSubmit(ev));
+
     console.debug('my-test:', this);
+  }
+
+  async _run () {
+    return await this._handleSubmit();
+  }
+
+  get _data () {
+    return this.$$;
+  }
+
+  async _handleSubmit (ev) {
+    if (ev) { ev.preventDefault(); }
+
+    const FORM = ev ? ev.target : this.$$.form;
+
+    FORM.elements.content.value = this._getDocument(document);
+
+    const { data, res, req } = await this._fetchValidatorResult(FORM);
+
+    const { relevant, notRel, filterRE } = this._filterA11yRelevant(data.messages || []);
+
+    this.$$ = { ...this.$$, relevant, notRel, filterRE, data, res, req };
+
+    // console.debug('my-test, RES:', this.$$);
+
+    return this.$$;
+  }
+
+  async _fetchValidatorResult (FORM) {
+    const formData = new FormData(FORM);
+    // formData.append('showoutline', 'yes');
+    // formData.append('showimagereport', 'yes');
+
+    const req = new Request(FORM.action, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json'
+      },
+      body: formData
+    });
+    const res = await fetch(req);
+    const data = await res.json();
+
+    return { data, res, req };
   }
 
   _getDocument (elem) {
@@ -72,6 +123,22 @@ export class MyTestElement extends MyElement {
       elem = elem.nextSibling;
     }
     return doc;
+  }
+
+  _filterA11yRelevant (messages) {
+    const filterRE = filterStrings.join('|');
+    const notRel = [];
+
+    const relevant = messages.filter(it => {
+      const isRel = it.message.match(filterRE) !== null;
+
+      if (!isRel) {
+        notRel.push(it);
+      }
+      return isRel;
+    });
+
+    return { relevant, notRel, filterRE };
   }
 }
 
