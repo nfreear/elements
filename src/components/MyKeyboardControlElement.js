eedefd5e4162298ddf8b59d5cc7a78fe300476b8
Nick Freear <nfreear@yahoo.co.uk>
2023-01-17 21:24:11 +0000

    fix: Use up/down arrows with a listbox - `<my-keyboard-control>` (#37) (#10)

--- src/components/MyKeyboardControlElement.js
+++ src/components/MyKeyboardControlElement.js
@@ -1,13 +1,17 @@
 /**
- * Control a grid, listbox or similar widget with the arrows keys.
+ * Navigate a grid, listbox or similar widget using the arrows keys, etc.
  *
- * @TODO Currently needs left/right arrow keys for vertical listboxes.
+ * @TODO Maybe rename to <my-keyboard-nav> ?
+ * @TODO Currently auto-detects grid and listbox. Add auto-detect for more?
  *
  * @copyright © Nick Freear, 11-Jan-2023.
+ * @see https://w3.org/WAI/ARIA/apg/example-index/grid/dataGrids.html
  * @see ./MyDatePickerElement.js
  */
 
 import MyElement from '../MyElement.js';
+
+const { CustomEvent } = window;
 
 const HORIZ = {
   Left: -1,
@@ -21,14 +25,26 @@ const VERT = {
   Up: -1,
   Down: 1
 };
+const BOTH = {
+  Left: -1,
+  Right: 1,
+  Up: -1,
+  Down: 1
+};
 
 export class MyKeyboardControlElement extends MyElement {
   static getTag () {
     return 'my-keyboard-control';
   }
 
+  /** Are we in 1-Dimensional mode?
+   */
+  get _is1Dim () {
+    return this.getAttribute('is1dim') === 'true' || this._isListbox;
+  }
+
   get _isListbox () {
-    return this.dataset.role === 'listbox' || this.querySelector('[ role = listbox ]');
+    return this.dataset.role === 'listbox' || !!this.querySelector('[ role = listbox ]');
   }
 
   get _gridSelector () {
@@ -46,34 +62,34 @@ export class MyKeyboardControlElement extends MyElement {
 
     const GRID = this.querySelector(this._gridSelector);
 
-    const rows = this._isListbox ? [GRID] : GRID.querySelectorAll('tr');
+    const rows = this._is1Dim ? [GRID] : GRID.querySelectorAll('tr');
     const cells = GRID.querySelectorAll(this._cellSelector);
 
-    this.$$ = { grid: this._gridSelector, cell: this._cellSelector, selected, rows, cells, GRID };
+    this.$$ = { is1D: this._is1Dim, grid: this._gridSelector, cell: this._cellSelector, selected, rows, cells, GRID };
 
     this._initialize();
 
-    GRID.addEventListener('click', ev => this._clickHandler(ev));
-    GRID.addEventListener('keyup', ev => this._keyupHandler(ev));
+    this.addEventListener('click', ev => this._clickHandler(ev)); // Was: GRID.
+    this.addEventListener('keyup', ev => this._keyupHandler(ev));
 
     console.debug('my-keyboard-control:', this.$$, this);
   }
 
   _initialize () {
-    this.$$.rows.forEach((row, week) => {
-      row.setAttribute('data-week', week);
+    this.$$.rows.forEach((row, ridx) => {
+      row.setAttribute('data-row', ridx); // Was: 'week'
 
       const R_CELLS = row.querySelectorAll(this._cellSelector);
       // @WAS: const R_CELLS = row.querySelectorAll('td');
 
-      R_CELLS.forEach((cell, idx) => {
+      R_CELLS.forEach((cell, cidx) => {
         // cell.setAttribute('role', 'gridcell');
         if (cell.getAttribute('tabindex') !== '0') {
           cell.setAttribute('tabindex', -1);
         }
 
-        cell.setAttribute('data-col', idx);
-        cell.setAttribute('data-coord', `${idx},${week}`);
+        cell.setAttribute('data-col', cidx);
+        cell.setAttribute('data-coord', `${cidx},${ridx}`);
       });
     });
 
@@ -114,35 +130,59 @@ export class MyKeyboardControlElement extends MyElement {
 
     this._reset();
     this._setSelected(ev.target);
+    this._fireSelectedEvent(ev.target, null, null, ev);
 
     console.debug('click:', DATE, ev);
   }
 
   _keyupHandler (ev) {
     const COL = parseInt(ev.target.dataset.col) || 0;
-    const WEEK = parseInt(ev.target.parentElement.dataset.week) || 0;
+    const ROW = parseInt(ev.target.parentElement.dataset.row) || 0; // Aka "ROW"
 
     // const BEFORE = ev.target.textContent;
     const IS_ARROW = /Arrow/.test(ev.key);
     const DIR = ev.key.replace(/Arrow/, '');
+    const IS_1D = this._is1Dim;
 
     if (IS_ARROW) {
       ev.preventDefault();
 
-      const COORDS = `${COL + HORIZ[DIR]},${WEEK + VERT[DIR]}`;
+      const COORDS = `${COL + this._horiz[DIR]},${IS_1D ? 0 : (ROW + VERT[DIR])}`;
+      // @WAS: const COORDS = `${COL + HORIZ[DIR]},${WEEK + VERT[DIR]}`;
 
       const CELL = this.$$.GRID.querySelector(`[data-coord="${COORDS}"]`);
 
       if (CELL) {
         this._reset();
         this._setSelected(CELL);
+        this._fireSelectedEvent(CELL, DIR, COORDS, ev);
         CELL.focus();
       }
 
       // AFTER = BEFORE + step;
 
-      console.debug(`keyup: "${COORDS}"`, COL, WEEK, IS_ARROW, DIR, ev);
+      console.debug(`keyup: "${COORDS}"`, COL, ROW, IS_ARROW, DIR, ev);
     }
+  }
+
+  _fireSelectedEvent (cell, dir, coords, ev) {
+    const event = new CustomEvent('selected', {
+      detail: { cell, dir, coords, controls: this._getControlsElem(cell), origEvent: ev }
+    });
+    this.dispatchEvent(event);
+  }
+
+  _getControlsElem (cell) {
+    const CONTROLS = cell.getAttribute('aria-controls') || null;
+    return CONTROLS ? document.getElementById(CONTROLS) : null;
+  }
+
+  get _horiz () {
+    return this._is1Dim ? BOTH : HORIZ;
+  }
+
+  get _vert () {
+    return this._is1Dim ? BOTH : VERT;
   }
 }
 
