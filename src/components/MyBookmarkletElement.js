6c77f79f35e0b8123c5870f99b7d043f15f9b66a
Nick Freear <nfreear@yahoo.co.uk>
2025-04-16 17:14:53 +0100

    fix: Add `my-bookmarklet.css` stylesheet; Comments etc. in MyBookmarkletEl (#55)

--- src/components/MyBookmarkletElement.js
+++ src/components/MyBookmarkletElement.js
@@ -1,28 +1,31 @@
-/**
- * Embed a bookmarklet Javascript.
- *
- * @copyright © Nick Freear, 23-Jan-2022.
- *
- * @see ../demo/my-bookmarklet.html
- * @class MyBookmarkletElement
- */
 
 const { HTMLElement, location } = window;
 
+/**
+ * Embed a link to install a bookmarklet Javascript.
+ *
+ * @copyright © Nick Freear, 23-Jan-2022.
+ *
+ * @demo ../demo/my-bookmarklet.html
+ * @customElement my-bookmarklet
+ */
 export class MyBookmarkletElement extends HTMLElement {
-  static getTag () {
-    return 'my-bookmarklet';
-  }
+  static getTag () { return 'my-bookmarklet'; }
 
+  /** 'name' attribute.
+   *  @return {string}
+   */
   get name () { return this.getAttribute('name') || this.textContent || 'Bookmarklet'; }
 
   get originPlaceholder () { return '{__ORIGIN__}'; }
 
+  /** Load the bookmarklet source code into the link from a function.
+   *  @param {function} theFunction
+   */
   fromFunction (theFunction) {
     console.assert(typeof theFunction === 'function', 'Expecting function as parameter.');
     const EL = document.createElement('a');
-    const FUNC = theFunction.toString();
-    const BODY = FUNC.slice(FUNC.indexOf('{') + 1, FUNC.lastIndexOf('}'));
+    const BODY = this._extractFunctionBody(theFunction);
     const SCRIPT = this._fixScriptUrl(BODY);
 
     EL.href = `javascript:${SCRIPT}`;
@@ -32,11 +35,20 @@ export class MyBookmarkletElement extends HTMLElement {
     console.debug(`my-bookmarklet. From function - "${this.name}":`, SCRIPT);
   }
 
+  /** Get the body of the source function.
+   * @param {function} theFunction
+   * @return {string}
+   */
+  _extractFunctionBody (theFunction) {
+    const fnString = theFunction.toString();
+    return fnString.slice(fnString.indexOf('{') + 1, fnString.lastIndexOf('}'));
+  }
+
   _fixScriptUrl (script) {
     return script.replace(this.originPlaceholder, location.origin);
   }
 
-  async connectedCallback () {
+  connectedCallback () {
     console.debug('my-bookmarklet:', this.name, this);
   }
 }
