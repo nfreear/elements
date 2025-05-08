efed9cf6c3ba8163d5d6394847b68b8f9fd1fd2b
Nick Freear <nfreear@yahoo.co.uk>
2025-05-08 22:02:43 +0100

    task: Re-factor `scripts/copy.mjs` (#42)

--- scripts/copy.mjs
+++ scripts/copy.mjs
@@ -2,23 +2,28 @@
  * Copy source files to individual packages.
  */
 
-import { copyFile } from 'node:fs/promises';
+import { copyFile, readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+// import packagesData from './packages.json' with { type: 'json' };
 
-const base = 'packages';
-const PACKAGES = [
-  {
-    pkg: 'my-map',
-    paths: [
-      'src/MyElement.js',
-      'src/Options.js',
-      'src/components/MyMapElement.js'
-      // Deprecated: 'src/templates/my-map.tpl.html'
-    ]
-  }
-];
+const { base, packages } = await readJsonFile('packages.json');
 
-PACKAGES.forEach(async ({ pkg, paths }) => {
-  console.debug(`Copying ${paths.length} files for:`, pkg);
+packages.forEach(async ({ id }) => {
+  const { files } = await readJsonFile('..', base, id, 'package.json');
+  const paths = files.filter(file => !/^!/.test(file));
 
-  await paths.forEach(async (path) => await copyFile(path, `${base}/${pkg}/${path}`));
+  console.debug(`Copying ${paths.length} files for:`, id, paths);
+
+  await paths.forEach(async (path) => await copyFile(path, `${base}/${id}/${path}`));
 });
+
+async function readJsonFile (...pathParts) {
+  const filePath = resolvePath(...pathParts);
+  const json = await readFile(filePath, 'utf8');
+  return JSON.parse(json);
+}
+
+function resolvePath (...params) {
+  const __dirname = import.meta.dirname;
+  return resolve(__dirname, ...params);
+}
