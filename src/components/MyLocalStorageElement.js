7b69b0c9d91cf74935e8ed1d85197f8e74550bf2
Nick Freear <nfreear@yahoo.co.uk>
2025-05-14 17:32:29 +0100

    task: Re-factor `<my-local-storage>`, extending `HTMLElement` (#55)

--- src/components/MyLocalStorageElement.js
+++ src/components/MyLocalStorageElement.js
@@ -1,82 +1,84 @@
+
+const { HTMLElement } = window;
+
 /**
  * Easily store a form field value to `localStorage`.
  * Also supports `sessionStorage`.
  *
  * @copyright © Nick Freear, 24-Jan-2023.
- * @see https://codepen.io/nfreear/pen/OJoRbqa
+ * @customElement my-local-storage
+ * @demo https://codepen.io/nfreear/pen/OJoRbqa
  * @status experimental
  * @since 1.6.0
  */
+export class MyLocalStorageElement extends HTMLElement {
+  static getTag () { return 'my-local-storage'; }
 
-import { MyElement } from '../MyElement.js';
+  get _eventName () { return 'input'; }
 
-export class MyLocalStorageElement extends MyElement {
-  static getTag () {
-    return 'my-local-storage';
+  get selector () {
+    const _selector = this.getAttribute('selector');
+    console.assert(_selector, '"selector" (CSS selector) - Attribute required.');
+    return _selector;
   }
 
-  get storage () {
-    return window[this.storeTo];
+  get key () {
+    const _key = this.getAttribute('key');
+    console.assert(_key, '"key" (Storage key) - Attribute required.');
+    return _key;
   }
 
-  get storeTo () {
+  get storage () { return window[this._storeTo]; }
+
+  get _storeTo () {
     const isSession = this.getAttribute('session') === 'true';
     return `${isSession ? 'session' : 'local'}Storage`;
   }
 
-  get value () {
-    return this.storage.getItem(`${this.key}.value`);
-  }
+  get value () { return this.storage.getItem(`${this.key}.value`); }
 
   set value (val) {
-    // console.debug();
     this.storage.setItem(`${this.key}.value`, val);
     this.storage.setItem(`${this.key}.date`, new Date().toISOString());
   }
 
-  get key () {
-    return this.getAttribute('key');
+  get _element () {
+    const elem = document.querySelector(this.selector);
+    console.assert(elem, `Target element not found: ${this.selector}`);
+    return elem;
   }
 
-  async connectedCallback () {
-    if (!this.getAttribute('key')) {
-      throw new Error("Attribute 'key' is required.");
-    }
-
-    const selector = this.getAttribute('selector');
-    if (!selector) {
-      throw new Error("Attribute 'selector' is required.");
+  connectedCallback () {
+    if (!this.key || !this.selector) {
+      throw new Error('Missing required attribute(s)');
     }
 
     // For now, just one element!
-    const elem = document.querySelector(selector);
-    if (!elem) {
-      throw new Error('No linked element selected.');
+    if (!this._element) {
+      throw new Error('Element not found.');
     }
 
-    const value = this.value;
-    const storeTo = this.storeTo;
-    const storage = this.storage;
-    const $$ = { selector, value, storeTo, elem, storage };
+    this._elementValue = this.value;
 
-    this.setTargetValue(elem, value);
+    /* elem.addEventListener('change', ev => { }); */
 
-    /* elem.addEventListener('change', ev => {
-      const VALUE = this.getTargetValue(ev.target);
-      this.value = VALUE;
-      console.debug('Change event - stored value:', VALUE, ev);
-    }); */
+    this._element.addEventListener(this._eventName, (ev) => this._onInputEvent(ev));
 
-    elem.addEventListener('input', ev => {
-      const VALUE = this.getTargetValue(ev.target);
-      this.value = VALUE;
-      console.debug('Input event - stored value:', VALUE, ev);
-    });
-
-    console.debug('my-local-storage:', $$, this);
+    console.debug('my-local-storage:', [this]);
   }
 
-  getTargetValue (target) {
+  _onInputEvent (ev) {
+    this.store();
+    console.debug('Input event - stored value:', this.value, ev);
+  }
+
+  store () {
+    this.value = this._elementValue;
+    return this.value;
+  }
+
+  get _elementValue () {
+    const target = this._element;
     let value;
     if (target.type === 'checkbox') {
       value = target.checked ? target.value : 'false';
@@ -86,7 +88,8 @@ export class MyLocalStorageElement extends MyElement {
     return value;
   }
 
-  setTargetValue (target, value) {
+  set _elementValue (value) {
+    const target = this._element;
     if (target.type === 'checkbox') {
       target.checked = (value !== 'false');
     } else if (this._hasValue(target)) {
