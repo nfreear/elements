cbaea160dd6c3726c94f7f222fbac25ebe13ee68
Nick Freear <nfreear@yahoo.co.uk>
2026-01-16 17:28:48 +0000

    fix: Re-factor `MyTestElement` validation button (#61)(#63)

--- src/components/MyTestElement.js
+++ src/components/MyTestElement.js
@@ -1,3 +1,7 @@
+import attachTemplate from '../util/attachTemplate.js';
+
+const { fetch, FormData, Node, Request, HTMLElement } = window;
+
 /**
  * Run the W3C / Nu Html Checker on the serialized DOM.
  * Possibly other tests ...?
@@ -9,95 +13,76 @@
  * @status experimental, for demos, Codepen
  * @since 1.3.0
  */
+export class MyTestElement extends HTMLElement {
+  #validatorUrl = 'https://validator.w3.org/nu/';
+  #result = {};
 
-import { MyElement } from '../MyElement.js';
-
-const { fetch, FormData, Node, Request } = window;
-
-const filterStrings = ['tag seen', 'Stray end tag', 'Bad start tag', 'violates nesting rules', 'Duplicate ID', 'first occurrence of ID', 'Unclosed element', 'not allowed as child of element', 'unclosed elements', 'not allowed on element', 'unquoted attribute value', 'Duplicate attribute'];
-
-const TEMPLATE = `
-<template>
-  <slot></slot>
-  <style>
-    form { margin: 1rem 0; }
-    button { font: inherit; padding: .4rem .75rem; }
-    a, label { margin-right: .5rem; }
-    textarea { width: 8rem; }
-    [ name = out ] { width: 2rem; }
-  </style>
-  <form method="POST" action="https://validator.w3.org/nu/"
-    target="_blank" acceptCharset="utf-8" enctype="multipart/form-data">
-    <button type="submit">Check HTML! 💙</button>
-    <details>
-      <summary>(data)</summary>
-      <a href="https://validator.w3.org/nu/about.html">Validator</a>
-      <label>Source <input name="showsource" value="yes" checked type="checkbox"></label>
-      <label>Outline <input name="showoutline" value="yes" checked type="checkbox"></label>
-      <label>Output <input name="out" value="html"></label>
-      <label>Content <textarea name="content"></textarea></label>
-    </details>
-  </form>
-</template>
-`;
-
-export class MyTestElement extends MyElement {
   static getTag () {
     return 'my-test';
   }
 
+  get #autoRun () { return this.hasAttribute('auto-run'); }
+
+  get #filterStrings () {
+    return ['tag seen', 'Stray end tag', 'Bad start tag', 'violates nesting rules', 'Duplicate ID', 'first occurrence of ID', 'Unclosed element', 'not allowed as child of element', 'unclosed elements', 'not allowed on element', 'unquoted attribute value', 'Duplicate attribute'];
+  }
+
+  get #filterRegex () { return new RegExp(this.#filterStrings.join('|')); }
+
+  get #form () { return this.shadowRoot.querySelector('form'); }
+  get #elements () { return this.#form.elements; }
+
   async connectedCallback () {
     // Doesn't work!
-    const autoRun = this.getAttribute('auto-run') === 'true';
+    // const autoRun = this.getAttribute('auto-run') === 'true';
 
-    this.$$ = { autoRun };
+    attachTemplate(this.#htmlTemplate).to.shadowDOM(this);
+    // this._attachLocalTemplate(TEMPLATE);
 
-    this._attachLocalTemplate(TEMPLATE);
+    this.#elements.content.value = this.#getDocument(document);
 
-    this.$$.form = this.shadowRoot.querySelector('form');
-
-    this.$$.form.elements.content.value = this._getDocument(document);
+    // console.debug('Document:', this.#elements.content.value);
 
     // Facilitate Bootstrap ;).
-    this.style.display = 'block';
-    this.classList.add('container');
+    // this.style.display = 'block';
+    // this.classList.add('container');
 
-    // this.$$.form.addEventListener('submit', ev => this._handleSubmit(ev));
+    // this.#form.addEventListener('submit', ev => this.#handleSubmit(ev));
 
-    if (autoRun) {
+    if (this.#autoRun) {
       await this._run();
     }
 
-    console.debug('my-test:', this.$$, this);
+    console.debug('my-test:', this.#autoRun, [this]);
   }
 
   async _run (form) {
-    const { data, res, req } = await this._fetchValidatorResult(form || this.$$.form);
+    const { data, res, req } = await this.#fetchValidatorResult(form || this.#form);
 
-    const { accRelevant, accNotRel, filter } = this._filterAccessibilityRelevant(data.messages || []);
+    const { accRelevant, accNotRelevant } = this.#filterAccessibilityRelevant(data.messages || []);
 
-    this.$$.result = { accRelevant, accNotRel, all: data, filter, res, req };
+    this.#result = { accRelevant, accNotRelevant, all: data, res, req };
 
     // console.debug('my-test, run:', this.$$);
 
-    return this.$$.result;
+    return this.#result;
   }
 
   get _result () {
-    return this.$$.result;
+    return this.#result;
   }
 
-  async _handleSubmit (ev) {
+  async #handleSubmit (ev) {
     if (ev) { ev.preventDefault(); }
 
     const FORM = ev ? ev.target : this.$$.form;
 
-    FORM.elements.content.value = this._getDocument(document);
+    FORM.elements.content.value = this.#getDocument(document);
 
     return await this._run(FORM);
   }
 
-  async _fetchValidatorResult (FORM) {
+  async #fetchValidatorResult (FORM) {
     const formData = new FormData(FORM);
 
     formData.set('out', 'json');
@@ -117,7 +102,7 @@ export class MyTestElement extends MyElement {
     return { data, res, req };
   }
 
-  _getDocument (elem) {
+  #getDocument (elem) {
     /* eslint-disable-next-line */ // let doc;
     for (var doc = '', elem = elem.firstChild; elem;) {
       switch (elem.nodeType) {
@@ -142,19 +127,55 @@ export class MyTestElement extends MyElement {
     return doc;
   }
 
-  _filterAccessibilityRelevant (messages) {
-    const filterRE = filterStrings.join('|');
-    const accNotRel = [];
+  #filterAccessibilityRelevant (messages) {
+    const accNotRelevant = [];
 
     const accRelevant = messages.filter(it => {
-      const isRel = it.message.match(filterRE) !== null;
+      const isRelevant = this.#filterRegex.test(it.message);
+      // const isRel = it.message.match(this.#filterRegex) !== null;
 
-      if (!isRel) {
-        accNotRel.push(it);
+      if (!isRelevant) {
+        accNotRelevant.push(it);
       }
-      return isRel;
+      return isRelevant;
     });
 
-    return { accRelevant, accNotRel, filter: filterStrings };
+    return { accRelevant, accNotRelevant };
+  }
+
+  get #htmlTemplate () {
+    return `
+  <template>
+    <slot></slot>
+    <style>${this.#stylesheet}</style>
+    <form part="form" method="POST" action="${this.#validatorUrl}"
+      target="_blank" acceptCharset="utf-8" enctype="multipart/form-data">
+      <button part="button" type="submit">Check HTML!</button>
+      <details part="details">
+        <summary part="summary">(data)</summary>
+        <a part="a" href="https://validator.w3.org/nu/about.html">Validator</a>
+        <label>Source <input name="showsource" value="yes" checked type="checkbox"></label>
+        <label>Outline <input name="showoutline" value="yes" checked type="checkbox"></label>
+        <label>Output <input name="out" value="html"></label>
+        <label>Content <textarea name="content" part="textarea"></textarea></label>
+      </details>
+    </form>
+  </template>`;
+  }
+
+  get #stylesheet () {
+    return `
+  form { margin: 1rem 0; }
+  .X_button { font: inherit; padding: .4rem .75rem; }
+  button::after {
+    content: '💙';
+    margin-left: .25rem;
+  }
+  a, label { margin-right: .5rem; }
+  textarea { width: 8rem; }
+  [ name = out ] { width: 2rem; }
+    `;
   }
 }
+
+export default MyTestElement;
